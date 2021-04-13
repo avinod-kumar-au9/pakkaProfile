a5a7fa97e442cf72c95db1686a6b869c21151730
A vinod kumar <asundi.vinodkumar888@gmail.com>
2021-04-13 17:42:34 +0530

    done

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,46 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./App.css";
 
-const array = ["hey", "hello", "good", "awesome", "cool"];
-
 const HighFive = () => {
-  const [state, setstate] = useState({
-    inputidx: "",
-    err: "",
-  });
+  const [characters] = useState([
+    "R","A","N","D","O","M"
+  ]);
+  const firstinp = useRef(null);
+  const secondinp = useRef(null);
+  const thirdinp = useRef(null);
+  const fourthinp = useRef(null);
+  const fifthinp = useRef(null);
 
-  const inputHandler = (e, idx) => {
-    const filteredArray = array.filter((item) => item === e.target.value);
+  const charHandler = (char) => {
+    let inp1 = firstinp.current.value;
+    let inp2 = secondinp.current.value;
+    let inp3 = thirdinp.current.value;
+    let inp4 = fourthinp.current.value;
+    let inp5 = fifthinp.current.value;
 
-    if (filteredArray.length === 0) {
-      setstate({
-        ...state,
-        inputidx: idx,
-        err: "not matched",
-      });
-    } else {
-      setstate({
-        ...state,
-        inputidx: idx,
-        err: "matched",
-      });
+    if (inp1.length < 4) {
+      firstinp.current.value += char;
+    } else if (inp2.length < 5) {
+      secondinp.current.value += char;
+    } else if (inp3.length < 3) {
+      thirdinp.current.value += char;
+    } else if (inp4.length < 5) {
+      fourthinp.current.value += char;
+    } else if (inp5.length < 2) {
+      fifthinp.current.value += char;
     }
   };
 
-  const { inputidx, err } = state;
-
   return (
     <div>
-      {array.map((item, idx) => {
-        return (
-          <>
-            <label className="input">
-              <input onChange={(e) => inputHandler(e, idx)} key={idx} />
-              {idx === inputidx && err}
-            </label>
-          </>
-        );
-      })}
+      {characters.map((char, idx) => (
+        <button className="button" key={idx} onClick={() => charHandler(char)}>
+          {char}
+        </button>
+      ))}
+
+      <label className="input">
+        <input ref={firstinp} type="text"></input>4
+      </label>
+      <label className="input">
+        <input ref={secondinp} type="text"></input>5
+      </label>
+      <label className="input">
+        <input ref={thirdinp} type="text"></input>3
+      </label>
+      <label className="input">
+        <input ref={fourthinp} type="text"></input>5
+      </label>
+      <label className="input">
+        <input ref={fifthinp} type="text"></input>2
+      </label>
     </div>
   );
 };
